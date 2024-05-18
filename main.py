72dce7c41d2bd0178be9da79d00ca6e0e90df30f
Skstud5 <skripchenko@rb.asu.ru>
2024-05-18 22:50:47 +0700

    update main.py to run from uvicorn and debug

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
-from data_base.database import engine, Base
 from routers.routers import router as api_router
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
-
 app.include_router(api_router)
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
+
+uvicorn.run(app)
