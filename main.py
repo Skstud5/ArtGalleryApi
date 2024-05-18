556eef7a2b6939017fcc3afa0144f6d6bb703458
Skstud5 <skripchenko@rb.asu.ru>
2024-05-18 20:38:40 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("test commit")
+from fastapi import FastAPI
+from data_base.database import engine, Base
+from routers.routers import router as api_router
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(api_router)
